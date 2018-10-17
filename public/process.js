e28d3e8397873803b3142a53c52c5e4679d5346c
curioushoainam <hoainamtruongute@gmail.com>
2018-10-17 20:56:27 +0700

    Move contents JSX to a separated file

---
+++ public/process.js
@@ -0,0 +1,16 @@
+ReactDOM.render( 
+	<div>
+		<h1> I am inside the bable </h1> 
+		<h2 className="mauxanh"> Hallo leuter, Guten Natch </h2>	
+	</div>	
+	,document.getElementById('root')
+);
+
+ReactDOM.render( 
+	<div>
+		<h1> Guten Appetit </h1> 
+		<h2 className="mauxanh"> Ich hab euch alle lieb ! </h2>	
+	</div>
+		
+	,document.getElementById('root1')
+);
